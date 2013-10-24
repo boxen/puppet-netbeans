77edca7c66f5c0da38233732af136e13866b8321
Paul Healey <paulhealey@brighttalk.com>
2013-10-24 21:37:59 +0100

    Update to 7.4

--- manifests/jse.pp
+++ manifests/jse.pp
@@ -6,6 +6,6 @@
 class netbeans::jse {
   package { 'netbeans':
     provider => 'pkgdmg',
-    source   => 'http://download.netbeans.org/netbeans/7.3.1/final/bundles/netbeans-7.3.1-javase-macosx.dmg',
+    source   => 'http://dlc.sun.com.edgesuite.net/netbeans/7.4/final/bundles/netbeans-7.4-jse-macosx.dmg',
   }
 }
