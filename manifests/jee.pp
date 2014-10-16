e6941844c7b3eb23f8e2ddc0467edb794ed327d6
Alex Mace <a@m.me.uk>
2014-10-16 07:18:03 -0700

    Updated to 8.0.1

--- manifests/jee.pp
+++ manifests/jee.pp
@@ -6,6 +6,6 @@
 class netbeans::jee {
   package { 'netbeans':
     provider => 'pkgdmg',
-    source   => 'http://download.netbeans.org/netbeans/8.0/final/bundles/netbeans-8.0-javaee-macosx.dmg',
+    source   => 'http://download.netbeans.org/netbeans/8.0.1/final/bundles/netbeans-8.0.1-javaee-macosx.dmg',
   }
 }
